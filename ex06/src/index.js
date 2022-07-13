afcd20271b8c04f6371d6b68287254c00ea2912f
Ajla Fijuljanin <ajlafi@maestralsolutions.com>
2022-07-13 12:39:48 +0200

    Day04

---
+++ ex06/src/index.js
@@ -0,0 +1,14 @@
+function main(numOne, numTwo){
+    // only change code below this line
+    var myQuotient;
+    var numOne;
+    var numTwo;
+    myQuotient=2.5;
+    numOne=5.0;
+    numTwo=2.0;
+    // only change code above this line
+    return myQuotient;
+    }
+    console.log(main(5.0, 2.0)); // Change this line
+    module.exports = main;
+    
