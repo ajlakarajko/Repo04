aaf45fecd125a722fe6bebce0c34f3188d3a9b08
Ajla Fijuljanin <ajlafi@maestralsolutions.com>
2022-07-13 12:10:03 +0200

    Day04

---
+++ ex01/src/index1.js
@@ -0,0 +1,19 @@
+function main(numOne, numTwo){
+    var quotient;
+    if(numTwo === 0){
+    quotient = 'Divide by 0! Cannot divide by zero!';
+    } else {
+    // only change code below this line
+    var quotient;
+    var numOne;
+    var numTwo;
+    quotient=5;
+    numOne=15;
+    numTwo=3;
+    // only change code above this line
+    }
+    return quotient;
+    }
+    console.log(main(15, 3)); // change this line
+    module.exports = main;
+    
